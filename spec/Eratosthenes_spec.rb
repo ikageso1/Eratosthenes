91326924d4c789786c988816175d551312f6eb7d
tenma <1210370051g@kindai.ac.jp>
2015-08-08 06:54:52 +0000

    first commit

--- spec/Eratosthenes_spec.rb
+++ spec/Eratosthenes_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe Eratosthenes do
-  it 'has a version number' do
-    expect(Eratosthenes::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+	describe ""
 end
